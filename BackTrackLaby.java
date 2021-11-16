49b1b1f06316fb8afd968ee400de11f2e8688bd2
LorenzoACS <89931591+LorenzoACS@users.noreply.github.com>
2021-11-16 12:32:22 -0500

    Add files via upload

---
+++ BackTrackLaby.java
@@ -0,0 +1,14 @@
+public class BackTrackLaby {
+    public static int labSize = 0;
+    public static Labyrinth l;
+    public static void main(String[] args) {
+        int n = Integer.parseInt(args[0]);
+        labSize = n;
+        l = new Labyrinth(labSize, labSize);
+    }
+    public isValidMove(int row, int col) {
+        while (
+    }
+    
+    
+}
