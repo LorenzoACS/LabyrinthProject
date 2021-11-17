23c3b714f4489a908e8264487b3f59e0146f79ef
Moffi-bit <lorenzostiavelli6@gmail.com>
2021-11-16 20:54:23 -0500

    zz

--- BackTrackLaby.java
+++ BackTrackLaby.java
@@ -5,60 +5,68 @@ public class BackTrackLaby {
         int n = Integer.parseInt(args[0]);
         labSize = n;
     }
-    public boolean isValidMove(int row, int col) {
-        
-    }
-    
-    public boolean horizontalMove(int row, int col) {
-        int row2 = row;
-        int col2 = col;
-        while (l.isValid(row, col)) {
-            if (l.isStone(row, col)) {
-                return true;
-            } else {
-                return false;
+    public boolean isValidMove() {
+        for (int i = 0; i < labSize; i++) {
+            for (int j = 0; j < labSize; j++) {
+                if (l.isValid(i, j)) {
+                    return (l.isStone(i, j + 1) || l.isStone(i + 1, j) || l.isStone(i, j - 1) || l.isStone(i - 1, j));
+                } else {
+                    return false;
+                }
             }
-            col++;
-        }
-        while (l.isValid(row2, col2)) {
-            if (l.isStone(row2, col2)) {
-                return true;
-            } else {
-                return false;
-            }
-            col2--;
-        }
-    }
-    public boolean horizontalMove(int row, int col) {
-        int row2 = row;
-        int col2 = col;
-        while (l.isValid(row, col)) {
-            if (l.isStone(row, col)) {
-                return true;
-            } else {
-                return false;
-            }
-            row++;
-        }
-        while (l.isValid(row2, col2)) {
-            if (l.isStone(row2, col2)) {
-                return true;
-            } else {
-                return false;
-            }
-            row--;
         }
     }
     
-    public void findSafeMode(int row) {
-        if (row == labSize) {
-            return;
-        }
+    // public boolean horizontalMove(int row, int col) {
+        // int row2 = row;
+        // int col2 = col;
+        // while (l.isValid(row, col)) {
+            // if (l.isStone(row, col)) {
+                // return true;
+            // } else {
+                // return false;
+            // }
+            // col++;
+        // }
+        // while (l.isValid(row2, col2)) {
+            // if (l.isStone(row2, col2)) {
+                // return true;
+            // } else {
+                // return false;
+            // }
+            // col2--;
+        // }
+    // }
+    // public boolean horizontalMove(int row, int col) {
+        // int row2 = row;
+        // int col2 = col;
+        // while (l.isValid(row, col)) {
+            // if (l.isStone(row, col)) {
+                // return true;
+            // } else {
+                // return false;
+            // }
+            // row++;
+        // }
+        // while (l.isValid(row2, col2)) {
+            // if (l.isStone(row2, col2)) {
+                // return true;
+            // } else {
+                // return false;
+            // }
+            // row--;
+        // }
+    // }
+    
+    // public void findSafeMode(int row) {
+        // if (row == labSize) {
+            // return;
+        // }
         
-        for (int col = 0; col < labSize; col++) {
-            if (isValidMove(row, col)) {
+        // for (int col = 0; col < labSize; col++) {
+            // if (isValidMove(row, col)) {
                 
-            }
-        }
-    }
+            // }
+        // }
+    // }
 }
