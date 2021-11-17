3cc9239db7f8b1cc2e5734b64d9daf3222755790
Stiavelli <lstiavelli@friendsbalt.org>
2021-11-17 13:32:07 -0500

    updated

--- BackTrackLaby.java
+++ BackTrackLaby.java
@@ -1,72 +1,51 @@
+import java.util.ArrayList;
 public class BackTrackLaby {
-    public static int labSize = 0;
-    public static Labyrinth l = new Labyrinth(labSize, labSize);
-    public static void main(String[] args) {
-        int n = Integer.parseInt(args[0]);
-        labSize = n;
+
+   public static int labRow = 0;
+   public static int labCol = 0;
+   public static boolean[][] alreadyBeenHere;
+   public ArrayList<int[]> solution = new ArrayList<int[]>();
+   public static BackTrackLaby btl = new BackTrackLaby();
+
+   public static void main(String[] args) {
+       int row = Integer.parseInt(args[0]);
+       int col = Integer.parseInt(args[1]);
+       labRow = row;
+       labCol = col;
+       Labyrinth l = new Labyrinth(labRow, labCol);
+       alreadyBeenHere = new boolean[labRow][labCol];
+       btl.solve(l);
     }
-    public boolean isValidMove() {
-        for (int i = 0; i < labSize; i++) {
-            for (int j = 0; j < labSize; j++) {
-                if (l.isValid(i, j)) {
-                    return (l.isStone(i, j + 1) || l.isStone(i + 1, j) || l.isStone(i, j - 1) || l.isStone(i - 1, j));
-                } else {
-                    return false;
-                }
+    
+    public int[][] solve(Labyrinth l) {
+        findSafeMove(0, 0, l);
+        int[][] solutionArray = solution.toArray(new int[solution.size()][2]);
+        return solutionArray;
+    }
+    
+    public boolean isASafeMove(int row, int col, int[] x, Labyrinth l) {
+        return (l.isValid(row + x[0], col + x[1]) && l.isStone(row + x[0], col + x[1]) && !alreadyBeenHere[row + x[0]][col + x[1]]);
+    }
+    
+    public void makeMove(int[] x) {
+        solution.add(x);
+    }
+    
+    public void undoMove(int[] x) {
+        solution.remove(solution.size() - 1);
+    }
+    
+    public void findSafeMove(int row, int col, Labyrinth l) {
+        if (row == labRow - 1 && col == labCol - 1) {
+            
+            return;
+        } 
+        for (int[] x : new int[][] {Labyrinth.UP, Labyrinth.DOWN, Labyrinth.LEFT, Labyrinth.RIGHT}) {
+            if (isASafeMove(row, col, x, l)) {
+                makeMove(x);
+                findSafeMove(row + x[0], col + x[1], l);
+                undoMove(x);
             }
         }
     }
-    
-    // public boolean horizontalMove(int row, int col) {
-        // int row2 = row;
-        // int col2 = col;
-        // while (l.isValid(row, col)) {
-            // if (l.isStone(row, col)) {
-                // return true;
-            // } else {
-                // return false;
-            // }
-            // col++;
-        // }
-        // while (l.isValid(row2, col2)) {
-            // if (l.isStone(row2, col2)) {
-                // return true;
-            // } else {
-                // return false;
-            // }
-            // col2--;
-        // }
-    // }
-    // public boolean horizontalMove(int row, int col) {
-        // int row2 = row;
-        // int col2 = col;
-        // while (l.isValid(row, col)) {
-            // if (l.isStone(row, col)) {
-                // return true;
-            // } else {
-                // return false;
-            // }
-            // row++;
-        // }
-        // while (l.isValid(row2, col2)) {
-            // if (l.isStone(row2, col2)) {
-                // return true;
-            // } else {
-                // return false;
-            // }
-            // row--;
-        // }
-    // }
-    
-    // public void findSafeMode(int row) {
-        // if (row == labSize) {
-            // return;
-        // }
-        
-        // for (int col = 0; col < labSize; col++) {
-            // if (isValidMove(row, col)) {
-                
-            // }
-        // }
-    // }
 }
