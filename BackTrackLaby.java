3882dd09f293819b026c43765436cd0e7d57679c
Stiavelli <lstiavelli@friendsbalt.org>
2021-11-19 10:10:52 -0500

    still need to debug error

--- BackTrackLaby.java
+++ BackTrackLaby.java
@@ -1,13 +1,12 @@
 import java.util.ArrayList;
 public class BackTrackLaby {
 
-   public static int labRow = 0;
-   public static int labCol = 0;
+   public static int labRow;
+   public static int labCol;
    public static boolean[][] alreadyBeenHere;
    public static int[][] solutionArray; 
    public static ArrayList<int[]> solution = new ArrayList<int[]>();
    public static BackTrackLaby btl = new BackTrackLaby();
-   public int count = 0;
 
    public static void main(String[] args) {
        int row = Integer.parseInt(args[0]);
@@ -17,6 +16,7 @@ public class BackTrackLaby {
        Labyrinth l = new Labyrinth(labRow, labCol);
        alreadyBeenHere = new boolean[labRow][labCol];
        alreadyBeenHere[0][0] = true;
+       l.printGrid();
        System.out.println("\n" + l.solves(btl.solve(l)));
        l.printGrid();
     }
@@ -24,6 +24,17 @@ public class BackTrackLaby {
     public int[][] solve(Labyrinth l) {
         findSafeMove(0, 0, l);
         solutionArray = solution.toArray(new int[solution.size()][2]);
+        for (int i = 0; i < solution.size(); i++) {
+            if (solution.get(i) == Labyrinth.UP) {
+                    System.out.println(0);
+                } else if (solution.get(i) == Labyrinth.DOWN) {
+                    System.out.println(1);
+                } else if (solution.get(i) == Labyrinth.LEFT) {
+                    System.out.println(2);
+                } else if (solution.get(i) == Labyrinth.RIGHT) {
+                    System.out.println(3);
+                }
+        }
         return solutionArray;
     }
     
@@ -32,28 +43,19 @@ public class BackTrackLaby {
     }
 
     public void findSafeMove(int row, int col, Labyrinth l) {
-        if (row == labRow - 1 && col == labCol - 1) {
+        if (row == labRow - 1 && col == labCol - 1 && alreadyBeenHere[labRow - 1][labCol - 1]) {
+            System.out.println("Done");
             return;
         } 
-        
         for (int[] x : new int[][] {Labyrinth.UP, Labyrinth.DOWN, Labyrinth.LEFT, Labyrinth.RIGHT}) {
             if (isASafeMove(row, col, x, l)) {
                 solution.add(x);
                 alreadyBeenHere[row + x[0]][col + x[1]] = true;
-                if (solution.get(count) == Labyrinth.UP) {
-                    System.out.println(0 + " " + row + "," + col);
-                } else if (solution.get(count) == Labyrinth.DOWN) {
-                    System.out.println(1 + " " + row + "," + col);
-                } else if (solution.get(count) == Labyrinth.LEFT) {
-                    System.out.println(2 + " " + row + "," + col);
-                } else if (solution.get(count) == Labyrinth.RIGHT) {
-                    System.out.println(3 + " " + row + "," + col);
-                }
-                count++; 
                 findSafeMove(row + x[0], col + x[1], l);
-                //solution.remove(solution.size() - 1);
-                //alreadyBeenHere[row + x[0]][col + x[1]] = false;
-                count--;
+                if (!alreadyBeenHere[labRow - 1][labCol - 1]) {
+                    solution.remove(solution.size() - 1);
+                    alreadyBeenHere[row + x[0]][col + x[1]] = false;
+                }
             }
         }
     }
